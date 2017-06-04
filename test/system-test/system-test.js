c0b4a66e010d6be23a6b3776d4e642425fe77282
David Losert <david.losert@gmx.de>
2017-06-04 18:19:29 +0200

    [chore] Prepared a system-test filer

--- test/system-test/system-test.js
+++
@@ -1,8 +0,0 @@
-/**
- * This test will actually open a connection to the Cart and execute the tests directly.
- * Only works with a working cart and correct data.
- */
-
-describe('system-test', () => {
-
-});
---
+++ test/system-test/system-test.ts
@@ -0,0 +1,24 @@
+import {CartServer} from '../../src/cartServer';
+import {CartControl} from '../../src/cartControl';
+/**
+ * This test will actually open a connection to the Cart and execute the tests directly.
+ * Only works with a working cart and correct data.
+ */
+const CONFIG = {
+  PORT: 9000,
+  HOST: ''
+};
+
+describe('system-test', () => {
+  let cartServer;
+  let cartControl;
+  before((done) => {
+	cartServer = new CartServer(CONFIG.HOST, CONFIG.PORT);
+	cartControl = new CartControl(cartServer);
+	cartServer.connect(() => {
+	});
+  });
+  after((done) => {
+     cartServer.disconnect();
+  })
+});
